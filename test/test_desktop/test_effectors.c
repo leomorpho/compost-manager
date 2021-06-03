f009171bd495fa13a97a357f507a47f87d9eb1ec
leonard audibert <leonard.audibert@gmail.com>
2021-06-02 21:36:22 -0700

    Unit test importing main.h

---
+++ test/test_desktop/test_effectors.c
@@ -0,0 +1,15 @@
+#include <unity.h>
+#include <main.h>
+
+void test_high_temperature() {
+
+}
+
+int main( int argc, char **argv) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_high_temperature);
+
+    UNITY_END();
+    
+}
